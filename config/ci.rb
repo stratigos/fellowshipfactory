99f181cda59bcf0fc804ee774507770c66f79136
Todd Morningstar <techarchist@gmail.com>
2026-02-04 14:54:53 -0500

    Remove Vanilla Test Components ⚗️
    We want the app as focused as possible, and to remove any application
    tools or components that are not intended to be used or are unreachable.
    
    This changeset removes the `test/` directory and updates the CI files
    such that mentions to `test` or `system` are removed.
    
    GitHub #9

--- config/ci.rb
+++ config/ci.rb
@@ -9,8 +9,7 @@ CI.run do
   step "Security: Importmap vulnerability audit", "bin/importmap audit"
   step "Security: Brakeman code analysis", "bin/brakeman --quiet --no-pager --exit-on-warn --exit-on-error"
 
-  step "Tests: Rails", "bin/rails test"
-  step "Tests: System", "bin/rails test:system"
+  step "Tests: RSpec", "bin/rspec"
   step "Tests: Seeds", "env RAILS_ENV=test bin/rails db:seed:replant"
 
   # Optional: set a green GitHub commit status to unblock PR merge.
